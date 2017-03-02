86fbd8133e14cc8cc52222cabb043e8d9b04acb6
Daniel Enriquez <daniel.enriquez.montanez@intel.com>
2017-03-02 16:20:07 -0600

    * Fixed position of objects

--- Example/TestApp/Application.cpp
+++ Example/TestApp/Application.cpp
@@ -12,18 +12,32 @@
 
 #include "Application.h"
 
+enum {
+	SCENE = 0,
+	CROC,
+	LINK,
+	HOUSE_L,
+	HOUSE_R,
+	DRONE,
+	TOTAL_INSTANCES
+};
+
 void TestApp::InitVars() {
 	DtTimer.Init();
 	Position = XVECTOR3(0.0f, 0.0f, 0.0f);
-	Orientation = XVECTOR3(0.0f, 180.0f, 0.0f);
-	Scaling = XVECTOR3(0.05f, 0.05f, 0.05f);
+	Orientation = XVECTOR3(0.0f, 0.0f, 0.0f);
+	Scaling = XVECTOR3(1.0f, 1.0f, 1.0f);
 	SelectedMesh = 0;
 
 	Cam.Init(XVECTOR3(0.0f, 1.0f, 10.0f), Deg2Rad(45.0f), 1280.0f / 720.0f, 1.0f, 10000.0f);
 	Cam.Speed = 250.0f;
+	Cam.Eye = XVECTOR3(-18.804996f, 38.739578f, -250.309113f);
+	Cam.Pitch = 0.060000f;
+	Cam.Roll  = 0.0f;
+	Cam.Yaw = 0.02000f;
+	Cam.Update(0.0f);
 
-	XVECTOR3 Target = XVECTOR3(0.0f, 0.0f, 0.0f);
-	Cam.SetLookAt(Target);
+
 	FirstFrame = true;
 }
 
@@ -33,17 +47,23 @@ void TestApp::CreateAssets() {
 	int index = PrimitiveMgr.CreateMesh("Models/Scene.X");
 	Pigs[0].CreateInstance(PrimitiveMgr.GetPrimitive(index), &VP);
 
+	
 	index = PrimitiveMgr.CreateMesh("Models/NuCroc.X");
 	Pigs[1].CreateInstance(PrimitiveMgr.GetPrimitive(index), &VP);
+	
 
 	index = PrimitiveMgr.CreateMesh("Models/Link.X");
 	Pigs[2].CreateInstance(PrimitiveMgr.GetPrimitive(index), &VP);
 
 	index = PrimitiveMgr.CreateMesh("Models/House.X");
 	Pigs[3].CreateInstance(PrimitiveMgr.GetPrimitive(index), &VP);
+	Pigs[4].CreateInstance(PrimitiveMgr.GetPrimitive(index), &VP);
 
 	index = PrimitiveMgr.CreateMesh("Models/Drone.X");
-	Pigs[4].CreateInstance(PrimitiveMgr.GetPrimitive(index), &VP);
+	Pigs[5].CreateInstance(PrimitiveMgr.GetPrimitive(index), &VP);
+
+
+
 }
 
 void TestApp::DestroyAssets() {
@@ -58,26 +78,68 @@ void TestApp::OnUpdate() {
 	Cam.Update(DtSecs);
 	VP = Cam.VP;
 
-	PrimitiveInst *Sel = &Pigs[2];
+	Pigs[CROC].TranslateAbsolute(-43.871941f, 0.064795f, -58.153839f);
+	Pigs[CROC].RotateXAbsolute(0.0f);
+	Pigs[CROC].RotateYAbsolute(207.731613f);
+	Pigs[CROC].RotateXAbsolute(0.0f);
+	Pigs[CROC].ScaleAbsolute(0.151794f);
+	Pigs[CROC].Update();
+
+	Pigs[LINK].TranslateAbsolute(12.499269f, -0.079694f, -63.019135f);
+	Pigs[LINK].RotateXAbsolute(0.0f);
+	Pigs[LINK].RotateYAbsolute(-410.563721f);
+	Pigs[LINK].RotateXAbsolute(0.0f);
+	Pigs[LINK].ScaleAbsolute(0.114174f);
+	Pigs[LINK].Update();
+
+	Pigs[HOUSE_L].TranslateAbsolute(56.104416f, 0.574123f, 29.808973f);
+	Pigs[HOUSE_L].RotateXAbsolute(0.0f);
+	Pigs[HOUSE_L].RotateYAbsolute(141.33f);
+	Pigs[HOUSE_L].RotateXAbsolute(0.0f);
+	Pigs[HOUSE_L].ScaleAbsolute(0.670580f);
+	Pigs[HOUSE_L].Update();
+
+	Pigs[HOUSE_R].TranslateAbsolute(-82.823868f, 0.443788f, 34.599747f);
+	Pigs[HOUSE_R].RotateXAbsolute(0.0f);
+	Pigs[HOUSE_R].RotateYAbsolute(51.43101f);
+	Pigs[HOUSE_R].RotateXAbsolute(0.0f);
+	Pigs[HOUSE_R].ScaleAbsolute(0.670580f);
+	Pigs[HOUSE_R].Update();
+
+	static float xp = 0.0f, yp = 0.0f, zp = 0.0f, freq = 3.1415f, ampl = 180.0f;
+	xp =  ampl * sin(freq);
+	zp =  ampl * cos(freq);
+	yp = 10.0f * sin(freq*10.0f);
+	
+	Pigs[DRONE].TranslateAbsolute(xp, yp + 10.0f,zp);
+	Pigs[DRONE].RotateXAbsolute(0.0f);
+	Pigs[DRONE].RotateYAbsolute(Rad2Deg(freq));
+	Pigs[DRONE].RotateXAbsolute(0.0f);
+	Pigs[DRONE].ScaleAbsolute(0.670580f);
+	Pigs[DRONE].Update();
+
+	freq += DtSecs;
+
+/*
+	PrimitiveInst *Sel = &Pigs[DRONE];
 	Sel->TranslateAbsolute(Position.x, Position.y, Position.z);
 	Sel->RotateXAbsolute(Orientation.x);
 	Sel->RotateYAbsolute(Orientation.y);
 	Sel->RotateZAbsolute(Orientation.z);
 	Sel->ScaleAbsolute(Scaling.x);
 	Sel->Update();
-
+*/
 	OnDraw();
 }
 
 void TestApp::OnDraw() {
 	pFramework->pVideoDriver->Clear();
 	
-	Pigs[0].Draw();
-	Pigs[1].Draw();
-	Pigs[2].Draw();
-	Pigs[3].Draw();
-	Pigs[4].Draw();
+	for (int i = 0; i < TOTAL_INSTANCES; i++) {
+		Pigs[i].Draw();
+	}
 
+	
 	pFramework->pVideoDriver->SwapBuffers();
 	FirstFrame = false;
 }
@@ -87,70 +149,94 @@ void TestApp::OnInput() {
 	if (FirstFrame)
 		return;
 
+	bool changed = false;
 	const float speedFactor = 10.0f;
 	if (IManager.PressedKey(SDLK_UP)) {
 		Position.y += 1.0f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_DOWN)) {
 		Position.y -= 1.0f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_LEFT)) {
 		Position.x -= 1.0f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_RIGHT)) {
 		Position.x += 1.0f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_z)) {
 		Position.z -= 1.0f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_x)) {
 		Position.z += 1.0f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_KP_PLUS)) {
 		Scaling.x += 0.1f*speedFactor*DtSecs;
 		Scaling.y += 0.1f*speedFactor*DtSecs;
 		Scaling.z += 0.1f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_KP_MINUS)) {
 		Scaling.x -= 0.1f*speedFactor*DtSecs;
 		Scaling.y -= 0.1f*speedFactor*DtSecs;
 		Scaling.z -= 0.1f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_KP5)) {
 		Orientation.x -= 60.0f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_KP6)) {
 		Orientation.x += 60.0f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_KP2)) {
 		Orientation.y -= 60.0f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_KP3)) {
 		Orientation.y += 60.0f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_KP0)) {
 		Orientation.z -= 60.0f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_KP_PERIOD)) {
 		Orientation.z += 60.0f*speedFactor*DtSecs;
+		changed = true;
 	}
 
 	if (IManager.PressedKey(SDLK_KP_PERIOD)) {
 		Orientation.z += 60.0f*speedFactor*DtSecs;
+		changed = true;
 	}
+
+	bool displayInfo = false;
+	if (changed && displayInfo) {
+		printf("Position[%f,%f,%f] Rot[%f,%f,%f] Sc[%f]\n",Position.x,Position.y,Position.z,Orientation.x,Orientation.y,Orientation.z,Scaling.x);
+	}
+
+	//printf("Position[%f, %f, %f]\n\n", Cam.Eye.x, Cam.Eye.y, Cam.Eye.z);
+	//printf("Orientation[%f, %f, %f]\n\n", Cam.Pitch, Cam.Roll, Cam.Yaw);
 
 	if (IManager.PressedKey(SDLK_w)) {
 		Cam.MoveForward(DtSecs);
