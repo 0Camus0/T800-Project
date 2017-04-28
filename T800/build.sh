e011e82816efcb187b1e77d5ae179d3c024c598d
edenriqu <daniel.enriquez.montanez@intel.com>
2017-04-28 17:28:38 -0500

    Changed hardcoded path

--- T800/build.sh
+++ T800/build.sh
@@ -3,10 +3,8 @@
 BVERSION=$1
 
 
-
 if [ "$#" == 0 ];
 then
-echo "Default build is release..."
 BVERSION="release"
 fi
 
@@ -14,41 +12,15 @@ echo "Starting build... [$BVERSION]"
 
 #rm -f ./bin/AppLinux
 
-if [ "$BVERSION" == "debug" ];
-then
-echo "This is debug"
 pushd ./Framework > /dev/null
-./build.sh "debug"
+./build.sh $BVERSION
 popd > /dev/null
 echo "\n\n"
 pushd ./App > /dev/null
-./build.sh "debug"
-popd
-pushd ./bin > /dev/null
-chmod a+x ./AppLinux
-popd > /dev/null
-fi
-
-if [ "$BVERSION" == "release" ];
-then
-echo "This is release"
-pushd ./Framework > /dev/null
-./build.sh "release"
-popd > /dev/null
-echo "\n\n"
-pushd ./App > /dev/null
-./build.sh "release"
+./build.sh $BVERSION
 popd > /dev/null
 pushd ./bin > /dev/null
 chmod a+x ./AppLinux
 popd > /dev/null
-fi
-
-if [ ! -f ./bin/AppLinux ]; then
-    echo "Something wrong, exe not generated"
-else
-   echo "Sucess on /bin/AppLinux"
-fi
-
 
 
